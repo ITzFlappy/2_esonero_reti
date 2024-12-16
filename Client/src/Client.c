b9ec92a946564ed0df4d172e6660fa07e3237db2
FlappyPlay88 <barbarogerardo02@gmail.com>
2024-12-16 21:52:08 +0100

    trasformato da TCP a UDP

--- Client/src/Client.c
+++ Client/src/Client.c
@@ -14,198 +14,163 @@ int isValidNumber(char *);
 int checkChar(char *);
 void ShowMenu();
 
-int main(){
+int main() {
+#if defined WIN32
+    // Initialize Winsock for Windows
+    WSADATA wsa_data;
+    int result = WSAStartup(MAKEWORD(2, 2), &wsa_data);
+    if (result != NO_ERROR) {
+        printf("%s", "Start up failed");
+        return -1;
+    }
+    printf("%s", "Start up done correctly\n");
+#endif
 
-	#if defined WIN32
-		// Initialize Winsock for Windows
-		WSADATA wsa_data;
-		int result = WSAStartup(MAKEWORD(2, 2), &wsa_data);
-		if (result != NO_ERROR)
-		{
-			printf("%s", "Start up failed");
-			return -1;
-		}
-		printf("%s", "Start up done correctly\n");
-	#endif
+    // Socket creation
+    int client_socket;
+    client_socket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (client_socket < 0) {
+        errorhandler("Socket creation failed\n");
+        clearwinsock();
+        return -1;
+    }
+    printf("%s", "Socket created successfully\n");
 
-	// Socket creation
-	int client_socket;
-	client_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (client_socket < NO_ERROR)
-	{
-		errorhandler("socket creation failed\n");
-		closesocket(client_socket);
-		clearwinsock();
-		return -1;
-	}
-	printf("%s", "Socket created successfully\n");
+    // Setting up the server address structure
+    char *address_to_connect = "127.0.0.1";
+    int port = PROTOPORT;
+    struct sockaddr_in server_ad;
+    memset(&server_ad, 0, sizeof(server_ad)); // Clear structure memory
+    server_ad.sin_family = AF_INET;
+    server_ad.sin_addr.s_addr = inet_addr(address_to_connect);
+    server_ad.sin_port = htons(port);
 
-	// Setting up the server address structure
-	char * address_to_connect = "127.0.0.1";
-	int port = PROTOPORT;
-	struct sockaddr_in server_ad;
-	memset(&server_ad, 0, sizeof(server_ad)); // Clear structure memory
-	server_ad.sin_family = AF_INET;
-	server_ad.sin_addr.s_addr = inet_addr(address_to_connect);
-	server_ad.sin_port = htons(port);
+    printf("%s ", "Trying to send data to");
+    SetColor(1);
+    printf("%s:%d\n", address_to_connect, port);
+    SetColor(7);
 
-	printf("%s ", "Trying to establish connection with");
-	SetColor(1);
-	printf("%s:%d\n",address_to_connect, port);
-	SetColor(7);
+    // Allocate buffer for input data
+    char *input_string = malloc(BUFFERSIZE);
+    memset(input_string, 0, BUFFERSIZE);
 
-	// Establish connection with the server
-	int server_connect;
-	server_connect = connect(client_socket, (struct sockaddr *)&server_ad, sizeof(server_ad));
-	if (server_connect < NO_ERROR)
-	{
-		errorhandler("Failed to connect to the server\n");
-		closesocket(client_socket);
-		clearwinsock();
-		return -1;
-	}
-	printf("%s", "Connected with the server\n");
+    while (1) {
+        memset(input_string, 0, BUFFERSIZE);
 
-	// Allocate buffer for input data
-	char * input_string = malloc(BUFFERSIZE);
-	memset(input_string, 0, BUFFERSIZE);
+        // Display options for generating different types of passwords
+        ShowMenu();
 
-	// Receive response from server
-	int bytes_received = 0;
+        SetColor(10);
+        fgets(input_string, BUFFERSIZE - 1, stdin); // Get user input with a size limit
+        SetColor(7);
 
-	char server_string[BUFFERSIZE];
-	memset(server_string, 0, BUFFERSIZE);
-	bytes_received = recv(client_socket, server_string, BUFFERSIZE - 1, 0);
+        // Remove newline character at the end of input if it exists
+        int string_len = strlen(input_string);
+        if ((string_len > 0) && (input_string[string_len - 1] == '\n')) {
+            input_string[string_len - 1] = '\0';
+        }
 
+        char *string_tokens[3]; // Array to hold tokenized parts of input
+        char input_string_cpy[BUFFERSIZE];
+        strcpy(input_string_cpy, input_string);
+        tokenizer(string_tokens, input_string_cpy); // Tokenize the input
 
-	if (bytes_received > 0) {
-	            server_string[bytes_received] = '\0'; // Check the string is terminated
-	        } else if (bytes_received <= 0) {
-	            errorhandler("Retrieve failed or connection closed prematurely\n");
-	            closesocket(client_socket);
-	            clearwinsock();
-	            return -1;
-	        }
+        // Check if the input is valid
+        if (strcmp(string_tokens[0], "e") == 0) {
+            printf("Invalid input. Please enter a valid character and length.\n");
+            Sleep(1500);
+            system("CLS");
+            continue; // Ask for input again
+        }
 
-		while (1) {
-			memset(input_string, 0, BUFFERSIZE);
+        // Check if client requested to quit
+        if (strcmp(string_tokens[0], "q") == 0) {
+            printf("%s\n", "Closing connection with server\n");
+            break;
+        }
 
-			// Display options for generating different types of passwords
-			ShowMenu();
+        // Check for correct input format and values
+        int tokens_number = atoi(string_tokens[2]);
+        if (tokens_number != 2 || !checkChar(string_tokens[0]) || isValidNumber(string_tokens[1])) {
+            printf("%s", "You must send to server a valid character\n");
+            Sleep(1500);
+            system("CLS");
+            continue; // Ask for input again
+        }
 
-			SetColor(10);
-			fgets(input_string, BUFFERSIZE - 1, stdin); // Get user input with a size limit
-			SetColor(7);
+        // Send data to the server
+        int data_sent;
+        data_sent = sendto(client_socket, input_string, string_len, 0, (struct sockaddr *)&server_ad, sizeof(server_ad));
+        if (data_sent != string_len) {
+            errorhandler("Different number of bytes has been sent to the server\n");
+            closesocket(client_socket);
+            clearwinsock();
+            return -1;
+        }
+        printf("%s", "Data sent successfully\n");
 
-			// Remove newline character at the end of input if it exists
-			int string_len = strlen(input_string);
-			if ((string_len > 0) && (input_string[string_len - 1] == '\n')) {
-				input_string[string_len - 1] = '\0';
-			}
+        // Receive response from server
+        int bytes_received = 0;
+        char server_string[BUFFERSIZE];
+        memset(server_string, 0, BUFFERSIZE);
+        int server_len = sizeof(server_ad); // Length of the server address structure
+        bytes_received = recvfrom(client_socket, server_string, BUFFERSIZE - 1, 0, (struct sockaddr *)&server_ad, &server_len);
+        if (bytes_received <= 0) {
+            errorhandler("Retrieve failed or connection closed prematurely\n");
+            closesocket(client_socket);
+            clearwinsock();
+            return -1;
+        }
 
-			char *string_tokens[3]; // Array to hold tokenized parts of input
-			char input_string_cpy[BUFFERSIZE];
-			strcpy(input_string_cpy, input_string);
-			tokenizer(string_tokens, input_string_cpy); // Tokenize the input
+        SetColor(10);
+        printf("Password generated: %s\n", server_string);
+        SetColor(7);
 
-			// Check if the input is valid
-			if (strcmp(string_tokens[0], "e") == 0) {
-				printf("Invalid input. Please enter a valid character and length.\n");
-				Sleep(1500);
-				system("CLS");
-				continue; // Ask for input again
-			}
+        Sleep(3000);
+        system("CLS");
+        printf("%s%s\n%s%s\n", "Previous input -> ", input_string, "Result -> ", server_string);
+    }
 
-			// Check if client requested to quit
-			if (strcmp(string_tokens[0], "q") == 0) {
-				send(client_socket, input_string, string_len, 0);
-				printf("%s\n", "Closing connection with server\n");
-				break;
-			}
+    // Close socket and clean up Winsock
+    closesocket(client_socket);
+    clearwinsock();
+    printf("%s", "\n");
+    system("PAUSE");
 
-			// Check for correct input format and values
-			int tokens_number = atoi(string_tokens[2]);
-			if (tokens_number != 2 || !checkChar(string_tokens[0]) || isValidNumber(string_tokens[1])) {
-				printf("%s", "You must send to server a valid character\n");
-				Sleep(1500);
-				system("CLS");
-				continue; // Ask for input again
-			}
-
-			// Send data to the server
-			int data_sent;
-			data_sent = send(client_socket, input_string, string_len, 0);
-			if (data_sent != string_len) {
-				errorhandler("Different number of bytes has been sent to the server\n");
-				closesocket(client_socket);
-				clearwinsock();
-				return -1;
-			}
-			printf("%s", "Data sent successfully\n");
-
-			// Receive response from server
-			int bytes_received = 0;
-			char server_string[BUFFERSIZE];
-			memset(server_string, 0, BUFFERSIZE);
-			printf("Password generated: ");
-			bytes_received = recv(client_socket, server_string, BUFFERSIZE - 1, 0);
-			if (bytes_received <= 0) {
-				errorhandler("Retrieve failed or connection closed prematurely\n");
-				closesocket(client_socket);
-				clearwinsock();
-				return -1;
-			}
-
-			SetColor(10);
-			printf("%s\n", server_string);
-			SetColor(7);
-
-			Sleep(3000);
-			system("CLS");
-			printf("%s%s\n%s%s\n", "Previous input -> ", input_string, "Result -> ", server_string);
-		}
-
-	// Close socket and clean up Winsock
-	closesocket(client_socket);
-	clearwinsock();
-	printf("%s", "\n");
-	system("PAUSE");
-
-	return 0;
+    return 0;
 }
 
-
 // Clear the Winsock environment for Windows
-void clearwinsock(){
-	#if defined WIN32
-		WSACleanup();
-	#endif
+void clearwinsock() {
+#if defined WIN32
+    WSACleanup();
+#endif
 }
 
 // Error handler function
-void errorhandler(char * string){
-	printf("%s", string);
+void errorhandler(char *string) {
+    printf("%s", string);
 }
 
 // Tokenizer function to split input into tokens
-void tokenizer(char *tokens[3], char *string){
-	if (string == NULL || strlen(string) == 0) {
-	        printf("Error: Input is empty\n");
-	        tokens[0] = "e";  // Indicates an error
-	        return;
-	    }
+void tokenizer(char *tokens[3], char *string) {
+    if (string == NULL || strlen(string) == 0) {
+        printf("Error: Input is empty\n");
+        tokens[0] = "e";  // Indicates an error
+        return;
+    }
 
     char *type = string;
     short temp = 0;
     char *token_string;
     token_string = strtok(type, " ");
 
-    // Check if the toke_string is not null
-        if (token_string == NULL || strlen(token_string) != 1) {
-            printf("Error: Invalid or empty input\n");
-            tokens[0] = "e";  // Indicates an error
-            return;
-        }
+    // Check if the token_string is not null
+    if (token_string == NULL || strlen(token_string) != 1) {
+        printf("Error: Invalid or empty input\n");
+        tokens[0] = "e";  // Indicates an error
+        return;
+    }
 
     if (strcmp(token_string, "q") == 0) {
         tokens[0] = "q";
@@ -218,20 +183,19 @@ void tokenizer(char *tokens[3], char *string){
             token_string = strtok(NULL, " ");
         }
         if (temp == 2) {
-        	char str_temp_var[BUFFERSIZE];
-			snprintf(str_temp_var, BUFFERSIZE, "%d", temp);
-			tokens[temp] = str_temp_var;
+            char str_temp_var[BUFFERSIZE];
+            snprintf(str_temp_var, BUFFERSIZE, "%d", temp);
+            tokens[temp] = str_temp_var;
         }
     }
 }
 
-
 // Checks if the first character of input is a valid option for password type
-int checkChar(char * string){
-	if(string[0] == 'n' || string[0] == 'a' || string[0] == 'm' || string[0] == 's'){
-		return 1;
-	}
-	return 0;
+int checkChar(char *string) {
+    if (string[0] == 'n' || string[0] == 'a' || string[0] == 'm' || string[0] == 's') {
+        return 1;
+    }
+    return 0;
 }
 
 // Validates if input is a valid number and within the range for password length
